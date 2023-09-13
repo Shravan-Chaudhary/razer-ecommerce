fbf9f6d7890981adf0a5a6cba15685ff1f5bdba7
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-09-13 12:06:33 +0530

    setup redux toolkit

--- src/components/ProductCard2.tsx
+++ src/components/ProductCard2.tsx
@@ -1,5 +1,38 @@
-const ProductCard2 = () => {
-  return <div>ProductCard2</div>
+import { Icons } from './ui/icons'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Product } from '@/types'
+import Link from 'next/link'
+
+interface ProductCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  product: Product
+  variant?: 'default' | 'switchable'
+}
+
+const ProductCard2 = ({
+  product,
+  variant = 'default',
+  className,
+  ...props
+}: ProductCardProps) => {
+  return (
+    <Card>
+      <Link
+        href={`/product/${product.id}`}
+        aria-label={`View ${product.name} details`}
+      >
+        <CardHeader></CardHeader>
+      </Link>
+    </Card>
+  )
 }
 
 export default ProductCard2
